8d912d1a5706bfa9cabdc7f6ecf47f1f5f8a34d1
stephaniel96 <stephaniel96@gmail.com>
2015-11-14 21:48:17 -0500

    Started on array

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,20 @@ router.post('/', function(req, res, next) {
 	});
 
 	/* REST API - Search for past Tweets */
-	T.get('search/tweets', { q: req.body.hashtag, count: 100 }, function(err, data, response) {
-	  var hashtags = new Array();
-	  var hashtagarray = [];
+	T.get('search/tweets', { q: req.body.hashtag, count: 30}, function(err, data, response) {
+	  var hashtagCounts = new Array();
+
 	  for (x in data.statuses) {
 	    /*console.log(data.statuses[x].text);*/
 	    for (y in data.statuses[x].entities.hashtags) {
+
+	    	var htprelower = data.statuses[x].entities.hashtags[y].text;
+	    	var lowercase = htprelower.toLowerCase();
+	    	
+
+	    	hashtagCounts.push({hashtag: lowercase, count: 1});
+
+	    	/*
 	    	var htprelower = data.statuses[x].entities.hashtags[y].text;
 	    	var ht = htprelower.toLowerCase();
 	    	if (typeof hashtagarray[ht] == 'undefined')
@@ -36,14 +44,25 @@ router.post('/', function(req, res, next) {
 	    	else 
 	    		hashtagarray[ht]++;
 	    	
-	    	hashtags.push(ht);
+	    	hashtags.push(ht);*/
 	    	/*console.log(data.statuses[x].text);*/
 	    	/*console.log(ht);*/
 	    	/*console.log(hashtagarray[ht]);*/
 	    }
 	  }
-	  console.log(hashtagarray);
-	  res.render('index', { title: 'Express', hashtags: hashtags, search: req.body.hashtag});
+
+	  function compare(a, b)
+	  {
+	  	 if (a.count > b.count)
+	  	 	return -1;
+	  	 if (a.count < b.count)
+	  	 	return 1;
+	  	 return 0;
+	  }
+
+	  var new_array = hashtagCounts.sort(compare);
+	  console.log(new_array);
+	  res.render('index', { title: 'Express', hashtags: new_array, search: req.body.hashtag});
 	})
 });
 
